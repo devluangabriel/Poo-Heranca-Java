369dd8563bedeca3ff4371cdb592ba42c0f7aa49
devluangabriel <zuemeromualdo44@gmail.com>
2025-05-02 12:34:23 -0300

    adicionando comentarios

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,14 @@ public class Main {
 
         Clientes clientes = new Clientes();
 
-
+        //Acessando a class clientes que herda todos os métodos da classe Pessoa(Mãe).
         clientes.setNome("João");
         clientes.setAge(58);
+
+        //Acessando atributo da propria class clientes.
         clientes.setVoucher(5);
+
+        //Exibindo no console os dados setados.
         System.out.println(clientes.getNome());
         System.out.println(clientes.getAge());
         System.out.println(clientes.getVoucher());
